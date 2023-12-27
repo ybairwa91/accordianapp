56460f949e3f9d44bd15f7f6525b02d529315a23
Yogesh Kumar Bairwa <ybairwa786@gmail.com>
2023-12-27 11:30:40 +0530

    newProject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import React from "react";
 
-function App() {
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion />
     </div>
   );
 }
 
-export default App;
+function Accordion() {
+  return (
+    <div>
+      {faqs.map((element) => (
+        <AccordionItem
+          item={faqs[faqs.indexOf(element)].title}
+          text={faqs[faqs.indexOf(element)].text}
+          key={faqs.indexOf(element)}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, item, text }) {
+  return (
+    <div className="item">
+      {/* <div className="number">{num}</div> */}
+      <p className="text">{item}</p>
+      <p className="icon">{text}</p>
+    </div>
+  );
+}
